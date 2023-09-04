3d5c9183fc7fc74125c7914716be7dce16659e69
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2023-09-04 19:01:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,7 @@ pip install chatterbot==1.0.4
 pip install pytz --user
 pip install typing_extensions==4.4.0
 sudo apt install python
+sudo mkdir ~/nltk_data
+sudo chmod +755 /tokenizers/*
+sudo mv -r tokenizers ~/nltk_data/tokenizers
 python config.py
